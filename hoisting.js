52e6379ec0c5bc017889348520069f1a32ea331e
Sophie Shin <sophie@alloy.co>
2021-02-22 16:39:33 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,12 +1,11 @@
+var lyric = "maybe"
 function callMe() {
-var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
 }
-callMe();
 
 function crazy() {
   // fix the code in here:
