1623a137c0a7591dd18c33d98c21a18e7e374ee6
Sophie Shin <sophie@alloy.co>
2021-02-22 16:28:07 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,17 +1,16 @@
+var lyric = "maybe"
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
+thisIsCrazy() {
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
