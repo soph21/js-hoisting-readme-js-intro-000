058937777c204686d7e9a871f59cb59103487a92
Sophie Shin <sophie@alloy.co>
2021-02-22 16:47:39 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,9 +11,9 @@ function crazy() {
   // fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
-    thisIsCrazy()
   }
 }
+  thisIsCrazy()
 
 function sayMyName() {
   // fix the code in here:
